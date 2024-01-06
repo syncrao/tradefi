dc74642315feb745b0ee2a7948eea1dbcb692440
raoshah <shahrukhrao1@gmail.com>
2024-01-06 12:35:43 +0530

    second

--- api/app.py
+++ api/app.py
@@ -1,5 +1,14 @@
 from flask import Flask 
-
+import pyotp, json, http.client
+from appfunction import main_login
 
 app = Flask(__name__)
+app.secret_key = "srr"
+headers, jwt_token, api =  main_login()
+
+print(api)
+
+
+
+
 
