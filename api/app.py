67f4d71186146ca98644f0dc3a0a2ba4880bdf6b
raoshah <shahrukh1@gmail.com>
2024-05-30 23:40:12 +0530

    add backtest

--- api/app.py
+++ api/app.py
@@ -1,5 +1,5 @@
-from flask import Flask , render_template, jsonify
-import pyotp, json, http.client, os, threading, time
+from flask import Flask , render_template, request, jsonify
+import pyotp, json, http.client, os, threading, time, sqlite3
 from datetime import datetime, timedelta
 from dotenv import load_dotenv
 
@@ -11,6 +11,8 @@ app.secret_key = "srr"
 load_dotenv()
 
 api = os.getenv("api_key")
+
+
 conn = http.client.HTTPSConnection("apiconnect.angelbroking.com")
 
 def main_login():
@@ -83,9 +85,83 @@ def five_min(header, token, exc):
 
 
 
+def check_result(cdl , op , candle):
+    if op == 101:
+        if cdl < candle:
+            return True
+        else:
+            return False
+    elif op == 99:
+        if cdl > candle:
+            return True
+        else:
+            return False
+    else:
+        return "No Value"
+
+
+def chek(candle, list):
+    if len(candle) > len(list) and len(candle) > 4:
+        for data in list:
+            result = check_result(candle[data[0]][data[1]], data[2], candle[data[3]][data[4]])
+            if not result:
+                return False
+        return True
+    else:
+        return False
+
+def back(list):
+    connc = sqlite3.connect('fiveMinCandle.db') 
+    cursor = connc.cursor()
+    cursor.execute('SELECT * FROM candles')
+    rows = cursor.fetchall()
+    result_list = []
+    for row in rows:
+        id, date, candle = row
+        data_list = []
+        data = json.loads(candle)
+        entry = ""
+        for candle in data:
+            data_list.append(candle)
+            if entry == "":
+                result = chek(data_list, list)
+                if result: 
+                    entry = candle[4]
+                else:
+                    continue
+            else:
+                if candle[4] >  entry + 20:
+                    
+                    result_list.append(f"Date  : {date} - Entry Price : {entry} -  Exit Price: {candle[4]} - Book Profit {candle[4] - entry}")
+                    entry = ""
+                elif candle[4] <  entry - 10:
+                    result_list.append(f"Date  : {date} - Entry Price : {entry} -  Exit Price: {candle[4]} - Book Loss {candle[4] - entry}")
+                    entry = ""
+                elif len(data_list) > 71:
+                    result_list.append(f"Date  : {date} - Entry Price : {entry} -  Exit Price: {candle[4]} - Market Close {candle[4] - entry}")
+                    entry = ""
+                else:
+                    continue
+    return result_list
+
+
 @app.route('/')
 def index():
     return render_template("index.html")
+
+@app.route('/backtest')
+def backtest():
+    return render_template("backtest.html")
+
+
+@app.route('/backdata', methods=['POST'])
+def backdata():
+    data = request.json
+    list = [[int(item) for item in sublist] for sublist in data]
+    print(list) 
+    result_data = back(list)
+    return jsonify({'status': 'success', 'data': result_data})
+
 
 
 @app.route('/get_candle_data/<token>/<name>', methods=['GET'])
